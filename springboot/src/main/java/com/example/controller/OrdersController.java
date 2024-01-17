ce242f529c68c4e4f8afcef04d20ab4abca5fc9e
zhuangmengchao <ffg.zhuangmch@chinatelecom.cn>
2024-01-17 14:50:58 +0800

    补充，依赖更改和方法修复

--- springboot/src/main/java/com/example/controller/OrdersController.java
+++ springboot/src/main/java/com/example/controller/OrdersController.java
@@ -1,21 +1,22 @@
 package com.example.controller;
 
-import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.lang.Dict;
 import com.example.common.Result;
 import com.example.entity.Orders;
 import com.example.service.OrdersService;
 import com.github.pagehelper.PageInfo;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/orders")
--- springboot/src/main/java/com/example/controller/WebController.java
+++ springboot/src/main/java/com/example/controller/WebController.java
@@ -1,28 +1,20 @@
 package com.example.controller;
 
-import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.lang.Dict;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import com.example.common.Result;
 import com.example.common.enums.ResultCodeEnum;
 import com.example.common.enums.RoleEnum;
 import com.example.entity.Account;
-import com.example.entity.Orders;
-import com.example.entity.OrdersItem;
 import com.example.service.AdminService;
 import com.example.service.BusinessService;
-import com.example.service.OrdersItemService;
-import com.example.service.OrdersService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * 基础前端接口
